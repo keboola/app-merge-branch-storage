b0a5ddb5faf00d0cf86e81dcabea13ea90277440
Ondra <ondra.jodas@gmail.com>
2023-06-28 14:51:02 +0200

    ignore extra key

--- src/Configuration/SynchronizeConfigDefinition.php
+++ src/Configuration/SynchronizeConfigDefinition.php
@@ -9,4 +9,13 @@ use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 
 class SynchronizeConfigDefinition extends BaseConfigDefinition
 {
+    protected function getParametersDefinition(): ArrayNodeDefinition
+    {
+        $parametersNode = parent::getParametersDefinition();
+        // @formatter:off
+        /** @noinspection NullPointerExceptionInspection */
+        $parametersNode->ignoreExtraKeys();
+        // @formatter:on
+        return $parametersNode;
+    }
 }
