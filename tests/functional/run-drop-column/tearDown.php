8bbbcbf7eac9c0a759d52752364c334a6c221d45
Ondra <ondra.jodas@gmail.com>
2023-07-12 12:53:15 +0200

    functional test: run - drop column, primary keys, table, bucket

---
+++ tests/functional/run-drop-column/tearDown.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use Keboola\MergeBrancheStorage\Configuration\Config;
+use Keboola\StorageApi\Client;
+use Keboola\StorageApi\Components;
+use Keboola\StorageApi\Options\Components\Configuration;
+use Keboola\StorageApi\Options\Components\ConfigurationRow;
+use Keboola\StorageApi\Options\Components\ListComponentConfigurationsOptions;
+use Keboola\StorageApi\Options\Components\ListConfigurationRowsOptions;
+use PHPUnit\Framework\Assert;
+
+return function (Client $client, string $testDir): void {
+    $table = $client->getTable('in.c-testCreateBucket.testTable');
+
+    Assert::assertCount(1, $table['columns']);
+    Assert::assertEquals([
+        'column2',
+    ], $table['columns']);
+};
